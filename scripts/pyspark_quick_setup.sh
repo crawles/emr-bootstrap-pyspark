d3f0faca216060634a71243a942db11a35febf05
Chris Rawles <crawles@gmail.com>
2016-12-15 15:48:10 -0500

    vim bindings

--- scripts/pyspark_quick_setup.sh
+++ scripts/pyspark_quick_setup.sh
@@ -5,6 +5,18 @@ echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/co
 echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
 echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=$1'" >> /etc/spark/conf/spark-env.sh
 
+# git
+yum install git
+
 # add nbextensions
 pip install jupyter_contrib_nbextensions
 jupyter contrib nbextension install --user
+
+# vim bindings
+# Create required directory in case (optional)
+mkdir -p $(jupyter --data-dir)/nbextensions
+# Clone the repository
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+# Activate the extension
+jupyter nbextension enable vim_binding/vim_binding
